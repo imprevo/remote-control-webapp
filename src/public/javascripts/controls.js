522afa8d22bf8e23ea492c195cdf91addfec7a7a
imprevo <zhenek42@gmail.com>
2020-09-15 03:12:18 +0700

    moved client logc in init functions

--- src/public/javascripts/controls.js
+++ src/public/javascripts/controls.js
@@ -7,71 +7,75 @@ const sendRequest = (url, params) =>
     body: JSON.stringify(params),
   });
 
-const sendKeboard = (params) => sendRequest('/commands/keyboard', params);
+const initKeyboard = () => {
+  const sendKeboard = (params) => sendRequest('/commands/keyboard', params);
+  const mediaButtons = document.querySelectorAll('.button-media');
 
-const sendMouse = (params) => sendRequest('/commands/mouse', params);
-
-const mediaButtons = document.querySelectorAll('.button-media');
-
-mediaButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const key = button.dataset.key;
-    sendKeboard({ key });
+  mediaButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const { key } = button.dataset;
+      sendKeboard({ key });
+    });
   });
-});
+};
 
-const mousePanel = document.querySelector('.mouse-panel');
+const initMouse = () => {
+  const MOUSE_THRESHOLD = 10;
+  const MOUSE_SPEED = 2;
+  const sendMouse = (params) => sendRequest('/commands/mouse', params);
+  const mousePanel = document.querySelector('.mouse-panel');
+  let lastClientX = 0;
+  let lastClientY = 0;
+  let wasMoved = false;
 
-const MOUSE_THRESHOLD = 10;
-const MOUSE_SPEED = 2;
-let lastClientX = 0;
-let lastClientY = 0;
-let wasMoved = false;
+  mousePanel.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    const touch = e.touches[0];
 
-mousePanel.addEventListener('touchstart', (e) => {
-  e.preventDefault();
-  const touch = e.touches[0];
-
-  if (touch) {
-    wasMoved = false;
-    lastClientX = touch.clientX;
-    lastClientY = touch.clientY;
-  }
-});
-
-mousePanel.addEventListener('touchend', (e) => {
-  e.preventDefault();
-
-  if (!wasMoved) {
-    sendMouse({ key: 'mouseClickLeft' });
-  }
-
-  wasMoved = false;
-  lastClientX = 0;
-  lastClientY = 0;
-});
-
-mousePanel.addEventListener('touchmove', (e) => {
-  e.preventDefault();
-
-  const touch = e.touches[0];
-
-  if (touch) {
-    const deltaX = touch.clientX - lastClientX;
-    const deltaY = touch.clientY - lastClientY;
-
-    if (
-      Math.abs(deltaX) > MOUSE_THRESHOLD ||
-      Math.abs(deltaY) > MOUSE_THRESHOLD
-    ) {
-      wasMoved = true;
-      const x = deltaX * MOUSE_SPEED;
-      const y = deltaY * MOUSE_SPEED;
-
+    if (touch) {
+      wasMoved = false;
       lastClientX = touch.clientX;
       lastClientY = touch.clientY;
-
-      sendMouse({ key: 'mouseMove', input: { x, y } });
     }
-  }
-});
+  });
+
+  mousePanel.addEventListener('touchend', (e) => {
+    e.preventDefault();
+
+    if (!wasMoved) {
+      sendMouse({ key: 'mouseClickLeft' });
+    }
+
+    wasMoved = false;
+    lastClientX = 0;
+    lastClientY = 0;
+  });
+
+  mousePanel.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+
+    const touch = e.touches[0];
+
+    if (touch) {
+      const deltaX = touch.clientX - lastClientX;
+      const deltaY = touch.clientY - lastClientY;
+
+      if (
+        Math.abs(deltaX) > MOUSE_THRESHOLD ||
+        Math.abs(deltaY) > MOUSE_THRESHOLD
+      ) {
+        const x = deltaX * MOUSE_SPEED;
+        const y = deltaY * MOUSE_SPEED;
+
+        wasMoved = true;
+        lastClientX = touch.clientX;
+        lastClientY = touch.clientY;
+
+        sendMouse({ key: 'mouseMove', input: { x, y } });
+      }
+    }
+  });
+};
+
+initKeyboard();
+initMouse();
