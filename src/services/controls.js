909aa02a745aabfeefbddbd4a6292ec3fb233bfa
imprevo <zhenek42@gmail.com>
2020-09-15 01:17:54 +0700

    added vlc controls

--- src/services/controls.js
+++ src/services/controls.js
@@ -1,16 +1,24 @@
 const robot = require('robotjs');
 
 const supportedKeys = {
-  mediaStop: 'audio_stop',
-  mediaPlay: 'audio_play',
-  mediaPause: 'audio_pause',
-  mediaNext: 'audio_next',
-  mediaPrev: 'audio_prev',
+  mediaStop: () => robot.keyTap('audio_stop'),
+  mediaPlay: () => robot.keyTap('audio_play'),
+  mediaPause: () => robot.keyTap('audio_pause'),
+  mediaNext: () => robot.keyTap('audio_next'),
+  mediaPrev: () => robot.keyTap('audio_prev'),
+
+  vlcBackwardsShort: () => robot.keyTap('left', 'alt'),
+  vlcForwardShort: () => robot.keyTap('right', 'alt'),
+  vlcBackwardsMedium: () => robot.keyTap('left', 'control'),
+  vlcForwardMedium: () => robot.keyTap('right', 'control'),
+  vlcSubtitleNext: () => robot.keyTap('v'),
+  vlcAudioNext: () => robot.keyTap('b'),
+  vlcFullscreen: () => robot.keyTap('f'),
 };
 
 exports.keyTap = (key) => {
   if (!(key in supportedKeys)) {
     throw new Error('Operation not available');
   }
-  robot.keyTap(supportedKeys[key]);
+  supportedKeys[key]();
 };
